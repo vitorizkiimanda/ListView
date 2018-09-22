9e9e8226b5c12df3070e3875e64df1d5f4284e1f
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-09-22 22:24:03 +0700

    first push

---
+++ app/src/main/java/com/example/vitorizkiimanda/listview/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.vitorizkiimanda.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final String[] FRUITS = new String[] { "Apple", "Avocado", "Banana",
+            "Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
+            "Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple", "Apple", "Avocado", "Banana",
+            "Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
+            "Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple", "Apple", "Avocado", "Banana",
+            "Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
+            "Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple" };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.item_list_view, FRUITS);
+
+        //binding ListView
+        ListView listView = findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+    }
+}
